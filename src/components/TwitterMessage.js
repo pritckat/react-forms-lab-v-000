3eb991e78b0bcd61b0ce7bab76e006c4b893adc9
Katrina Pritchard <pritckat@gmail.com>
2018-11-14 20:44:36 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,16 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" onChange={this.state.message} />
       </div>
     );
   }
