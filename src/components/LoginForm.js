598a20025c5925ea61cec664184ddc7bfa199e45
Katrina Pritchard <pritckat@gmail.com>
2018-11-14 20:50:16 +0000

    automatically backed up by learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -10,19 +10,25 @@ class LoginForm extends React.Component {
     };
   }
 
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     return (
       <form>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleChange} value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange} value={this.state.password}/>
           </label>
         </div>
         <div>
